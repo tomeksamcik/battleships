22dbe0e48f29892f712aa90aed0effb0610e10c5
Tomasz Samcik <tomek@samcik.pro>
2019-01-28 19:12:52 +0100

    Test suite

---
+++ src/test/java/com/guestline/battleships/AllTests.java
@@ -0,0 +1,13 @@
+package com.guestline.battleships;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BoardTest.class, FieldTest.class,
+        PositionParametrizedTest.class, PositionTest.class, ShipTest.class,
+        UserInputTest.class })
+public class AllTests {
+
+}
